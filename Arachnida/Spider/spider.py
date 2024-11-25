3216184feccca7d9177b9ea2cd9c6dc65325f2ac
leon <leonjerineck@gmail.com>
2024-11-25 16:21:23 +0100

    ex00 parsing de fou

--- Arachnida/Spider/spider.py
+++ Arachnida/Spider/spider.py
@@ -1,5 +1,5 @@
 import sys
-import re
+import argparse
 
 
 def main():
@@ -14,62 +14,22 @@ def main():
         print(err)
 
 
-def fill_param(argv, i, data):
-    if 'r' in arg and not data['r']:
-        data['r'] = True
-    elif 'l' in arg and not data['l']:
-        data['l'] = int(argv[i + 1])
-    pass
+def has_duplicate(string):
+    return len(string) != len(set(string))
 
 
 def parser(argv):
-    data = {"url": "", "r": False, "l": None, "p": './data/'}
-    param_pattern = r"^-([rlp]{1,3})$"
-    nb_param = 0
-
-    for i in range(1, len(argv)):
-        if '-' in argv[i]:
-            if re.match(param_pattern, argv[i]) and no_duplicate(argv[i]):
-                fill_param(argv, i, data)
-            else:
-                print(argv[i])
-                raise AssertionError("Bad parameters")
-        elif i == len(argv) - 1:
-            data['url'] = argv[i]
-        print(nb_param)
-    return data
-
-
-# def check_all_param(argv):
-
-#     for i in range(1, len(argv)):
-#         if "-" in argv[i]:
-#             if re.match(param_pattern, argv[i]):
-#                 continue
-#             else:
-#                 raise AssertionError("Bad parameters")
-#     return 1
-
-
-# def check_r(argv, data):
-#     for i in range(1, len(argv)):
-#         if "-" in argv[i]:
-#             if argv[i] == "-r":
-#                 if not data['r']:
-#                     data['r'] = True
-#                     data['l'] = 5
-#                 else:
-#                     raise AssertionError("Bad parameters")
-#             else:
-
-
-# def check_l(argv, data):
-#     for i in range(1, len(argv)):
-#         if argv[i] == "-l" and i + 1 < len(argv) and argv[i + 1]:
-#             if not data['l'] or data['l'] == 5:
-#                 data['l'] = int(argv[i + 1])
-#             else:
-#                 raise AssertionError("Bad parameters")
+    parser = argparse.ArgumentParser(description="Get all the image from a given URL")
+    help_txt = "T/F if the program search for image in url recursively"
+    parser.add_argument('-r', action='store_true', help=help_txt)
+    help_txt = "Number of recursion"
+    parser.add_argument('-l', nargs='?', type=int, const=5, default=5, help=help_txt)
+    help_txt = "Where the image will be saved"
+    parser.add_argument('-p', nargs='?', type=str, const="./data/", default="./data/", help=help_txt)
+    help_txt = "Url where the img will be search"
+    parser.add_argument('url', type=str, help=help_txt)
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == '__main__':
